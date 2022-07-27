b0647dbb380dbce606dc60f676f7fc43ba7f9eb9
Yulei Chen <chenyuleiw@gmail.com>
2022-07-27 16:09:32 +0800

    feat: add import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,11 +2,12 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   
-  products: 'https://w0dhw8ytcl.execute-api.ap-northeast-2.amazonaws.com/dev/products'
+  // Attention that the region should be ap-northeast-2
+  products: 'https://w0dhw8ytcl.execute-api.ap-northeast-2.amazonaws.com/dev/products',
+  import: 'https://tm46xr7f5j.execute-api.ap-northeast-2.amazonaws.com/dev'
 };
 
 export default API_PATHS;
